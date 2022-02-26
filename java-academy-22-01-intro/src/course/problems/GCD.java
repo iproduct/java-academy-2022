7684ce219582c62f237fe9f62115f571d4881ffd
Trayan Iliev <office@iproduct.org>
2022-02-26 12:59:04 +0200

    examples added

--- java-academy-22-01-intro/src/course/problems/GCD.java
+++ java-academy-22-01-intro/src/course/problems/GCD.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class GCD {
     public static int getGCD(int a, int b){
-        return 0; //TODO Implement me!
+        return 0; //TODO Implement me : https://en.wikipedia.org/wiki/Euclidean_algorithm
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
