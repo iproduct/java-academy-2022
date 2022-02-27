386b73398d793425d450e224744b6f6ea147c6e0
Trayan Iliev <office@iproduct.org>
2022-02-27 10:10:21 +0200

    stop words added

--- java-academy-22-01-intro/src/course/academy/wordcount/WordCountDemo.java
+++ java-academy-22-01-intro/src/course/academy/wordcount/WordCountDemo.java
@@ -35,6 +35,22 @@ public class WordCountDemo {
             "due to unresolved security issues in older versions.[22] Oracle advises its users to " +
             "immediately transition to a supported version, such as one of the LTS versions (8, 11, 17).\n";
 
+    public static final String[] STOP_WORDS = {
+            "when", "didn't", "wasn", "y", "few", "below", "into", "there", "his", "these", "about", "if", "again",
+            "too", "were", "then", "doing", "haven", "such", "this", "me", "the", "further", "whom", "having",
+            "mightn", "both", "him", "she", "don", "yourselves", "all", "do", "it's", "i", "what", "needn", "had",
+            "during", "weren", "wasn't", "up", "will", "just", "isn", "theirs", "own", "ll", "hadn", "aren't",
+            "while", "hasn't", "themselves", "re", "in", "she's", "wouldn't", "he", "couldn't", "their", "more",
+            "that'll", "ain", "t", "a", "was", "but", "couldn", "only", "been", "so", "until", "have", "from",
+            "those", "or", "our", "ourselves", "as", "doesn", "over", "how", "you'd", "where", "itself", "against",
+            "very", "you'll", "who", "needn't", "by", "no", "each", "its", "at", "we", "any", "ours", "mightn't",
+            "through", "does", "here", "didn", "between", "some", "which", "other", "did", "don't", "hasn", "won",
+            "not", "should", "haven't", "on", "now", "with", "you", "them", "himself", "under", "you've", "yours",
+            "they", "shouldn", "doesn't", "of", "can", "be", "above", "weren't", "wouldn", "for", "s", "and",
+            "hadn't", "herself", "nor", "should've", "am", "once", "ve", "an", "hers", "myself", "to", "her", "is",
+            "shan't", "are", "same", "being", "your", "than", "it", "aren", "shan", "d", "m", "isn't", "down",
+            "shouldn't", "you're", "why", "o", "yourself", "has", "my", "ma", "out", "that", "mustn't", "because",
+            "after", "before", "most", "off", "mustn", "won't"};
     public static WordCount[] indexText(String text, int maxCount){
         String[] words = text.split(WORD_SPLITTING_PATTERN);
         System.out.println(Arrays.toString(words));
