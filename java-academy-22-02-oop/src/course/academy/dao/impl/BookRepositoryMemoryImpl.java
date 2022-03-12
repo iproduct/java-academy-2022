1772e0d3a283ad5efb5c83010af75be81c79b2ab
Trayan Iliev <office@iproduct.org>
2022-03-12 13:43:51 +0200

    Generic user repository added, IdGenerator externalized

--- java-academy-22-02-oop/src/course/academy/dao/impl/BookRepositoryMemoryImpl.java
+++ java-academy-22-02-oop/src/course/academy/dao/impl/BookRepositoryMemoryImpl.java
@@ -1,8 +1,14 @@
 package course.academy.dao.impl;
 
 import course.academy.dao.BookRepository;
+import course.academy.model.Book;
 
-public class BookRepositoryMemoryImpl extends AbstractPersistableBookRepository {
+public class BookRepositoryMemoryImpl extends AbstractPersistableRepository<Long, Book>
+        implements BookRepository {
+    public BookRepositoryMemoryImpl(IdGenerator<Long> idGenerator) {
+        super(idGenerator);
+    }
+
     @Override
     public void load() {
     }
--- java-academy-22-02-oop/src/course/academy/dao/impl/DaoFactoryMemoryImpl.java
+++ java-academy-22-02-oop/src/course/academy/dao/impl/DaoFactoryMemoryImpl.java
@@ -2,12 +2,13 @@ package course.academy.dao.impl;
 
 import course.academy.dao.BookRepository;
 import course.academy.dao.DaoFactory;
+import course.academy.dao.Repository;
 import course.academy.dao.UserRepository;
 
 public class DaoFactoryMemoryImpl implements DaoFactory {
     @Override
     public BookRepository createBookRepository() {
-        return new BookRepositoryMemoryImpl();
+        return new BookRepositoryMemoryImpl(new Repository.LongIdGenerator());
     }
 
     @Override
