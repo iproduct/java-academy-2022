57f27458333975e0d5e539b10fe6ee6e4691a87f
Trayan Iliev <office@iproduct.org>
2022-03-20 12:27:16 +0200

    StreamApiDemo03 added

--- java-academy-22-02-oop/src/course/demos/StreamApiDemo03.java
+++ java-academy-22-02-oop/src/course/demos/StreamApiDemo03.java
@@ -1,7 +1,18 @@
 package course.demos;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class StreamApiDemo03 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("data/fp_wiki.txt");
+        var lines = Files.lines(path, Charset.forName("utf-8"));
+        var words = lines.flatMap(line -> Stream.<String>empty());
+        words.forEach(System.out::println);
     }
 }
 
