e235f8bce40248610d390fc1afd625b6b7929aea
Trayan Iliev <office@iproduct.org>
2022-03-20 11:06:15 +0200

    StreamUtil with zip combiner function added

---
+++ java-academy-22-02-oop/src/course/demos/StreamApiDemo02.java
@@ -0,0 +1,42 @@
+package course.demos;
+
+import course.demos.util.Tuple2;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+public class StreamApiDemo02 {
+    public static <T> Stream<T> createStream(Collection<T> values) {
+        return values == null ? Stream.empty() : values.stream();
+    }
+
+    public static void main(String[] args) {
+        var empty = Stream.empty();
+        empty.forEach(System.out::println);
+//        empty.forEach(System.out::println);
+        var streamOfVals = Stream.of("hello", "functional", "java", "stream", "api");
+//        streamOfVals.forEach(System.out::println);
+//        System.out.println();
+        var streamOfVals2 = createStream(List.of("hello", "functional", "java", "stream", "api"));
+//        streamOfVals2.forEach(System.out::println);
+        var streamOfArrayVals =
+                Arrays.stream(new String[]{"hello", "functional", "java", "stream", "api"}, 1, 4);
+//        streamOfArrayVals.forEach(System.out::println);
+        var streamBuilded = Stream.<String>builder().add("hello").add("functional")
+                .add("java").add("stream").add("api")
+                .build();
+//        streamBuilded.forEach(System.out::println);
+        var streamGenerated = Stream.generate(() -> Math.random()).limit(100);
+//        streamGenerated.forEach(System.out::println);
+        var counter = new AtomicInteger();
+        var countedRandoms = Stream.generate(
+                        () -> new Tuple2(counter.incrementAndGet(), Math.random()))
+                .limit(100).parallel();
+        countedRandoms.forEach(tuple2 -> System.out.printf("%s -> %s%n", tuple2.getV1(),tuple2.getV2()));
+    }
+
+}
+
---
+++ java-academy-22-02-oop/src/course/demos/util/StreamUtil.java
@@ -0,0 +1,47 @@
+package course.demos.util;
+
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static java.util.Spliterator.SIZED;
+
+public class StreamUtil {
+    public static <A, B, C> Stream<C> zip(Stream<A> streamA, Stream<B> streamB, BiFunction<A, B, C> zipper) {
+        Objects.requireNonNull(zipper);
+        Spliterator<? extends A> aSpliterator = Objects.requireNonNull(streamA).spliterator();
+        Spliterator<? extends B> bSpliterator = Objects.requireNonNull(streamB).spliterator();
+
+        // Zipping looses DISTINCT and SORTED characteristics
+        int characteristics = ((aSpliterator.characteristics() & bSpliterator.characteristics()
+                & ~(Spliterator.DISTINCT | Spliterator.SORTED))
+                | (aSpliterator.characteristics() & SIZED | bSpliterator.characteristics() & SIZED));
+
+        long zipSize = (aSpliterator.getExactSizeIfKnown() >= 0) ?
+                ((bSpliterator.getExactSizeIfKnown() >= 0) ?
+                        Math.min(aSpliterator.estimateSize(), bSpliterator.estimateSize())
+                        : aSpliterator.estimateSize())
+                :bSpliterator.getExactSizeIfKnown();
+
+        final Iterator<A> iteratorA = Spliterators.iterator(aSpliterator);
+        final Iterator<B> iteratorB = Spliterators.iterator(bSpliterator);
+        final Iterator<C> iteratorC = new Iterator<C>() {
+            @Override
+            public boolean hasNext() {
+                return iteratorA.hasNext() && iteratorB.hasNext();
+            }
+
+            @Override
+            public C next() {
+                return zipper.apply(iteratorA.next(), iteratorB.next());
+            }
+        };
+        Spliterator<C> split = zipSize > 0 ? Spliterators.spliterator(iteratorC, zipSize, characteristics):
+                Spliterators.spliteratorUnknownSize(iteratorC, characteristics);
+        return StreamSupport.stream(split, streamA.isParallel() || streamA.isParallel());
+    }
+}
