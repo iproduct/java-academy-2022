78b83fe6d7e4ab5252ea063b09e2026611754c6e
Trayan Iliev <office@iproduct.org>
2022-04-02 11:01:19 +0300

    classes added from previous project

---
+++ 03-books-jdbc/src/main/java/course/academy/jdbc/JdbcSimpleDemo.java
@@ -0,0 +1,14 @@
+package course.academy.jdbc;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JdbcSimpleDemo {
+    public void run() {
+        log.info("Hello from Lombok!");
+    }
+    public static void main(String[] args) {
+        var demo = new JdbcSimpleDemo();
+        demo.run();
+    }
+}
